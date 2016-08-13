9456f534c7eb33787d9f7e0474f789b82e9c1ae3
Andrew R McHugh <arm5629@gmail.com>
2016-08-13 11:24:31 -0500

    Added notes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Medium Outliner - Work in progress
 
 > Chrome extension for seeing your document outline while writing on Medium
+
+### To Do - Make it work
+- Show outline on page
+- Differentiate between header levels
+- Show title and subtitle
+- Visually highlight TKs
+- Show word count
+- Show reading time
---
+++ medium-outliner.js
@@ -0,0 +1,20 @@
+//
+// Code notes
+//
+
+var headers3Array = document.querySelectorAll('h3');
+var headers4Array = document.querySelectorAll('h4');
+
+headers3 = [];
+headers4 = [];
+
+buildTextArray(headers3Array, headers3);
+buildTextArray(headers4Array, headers4);
+
+// Hmmm - should probably make an object, not array
+function buildTextArray(givenArray, outputArray){
+   for(i = 0; i < givenArray.length; i++){
+      var insertedText = givenArray[i].textContent;
+      outputArray.push(insertedText);
+   }
+}
